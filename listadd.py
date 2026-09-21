6304129e0275b03d7e9bf7ea3e894967e236d661
Sayali1503 <ingolesayali283@gmail.com>
2026-09-21 12:46:19 +0530

    Add list manipulation examples in listadd.py

---
+++ listadd.py
@@ -0,0 +1,9 @@
+#Adding Element in list
+l=[1,3,5,7,9]
+print("Original list:",l)
+l.append(6)
+print("Added Elements in List using append:",l)
+l.insert(2, 45)
+print("Added Elements in List using insert:",l)
+l.extend([21, 5, 67])
+print("Added Elements in List using extend:",l)
