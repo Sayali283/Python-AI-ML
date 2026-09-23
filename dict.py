c23e129518c6c281bd29761f47a98cd02ac5fd14
Sayali1503 <ingolesayali283@gmail.com>
2026-09-23 13:27:13 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,15 @@
+#Dictionary 
+d={
+    "name" :"Sayali",
+    "Roll no": 27,
+    "College":"Modern"
+}
+print(d)
+#Access Values
+print("Name :",d["name"])
+#Adding key Value Pair
+d["City"]="Pune"
+print("Adding Key Value in Dictionary:",d)
+#Updating a value
+d["College"]="Modern College"
+print("Updated Dictionary:",d)
