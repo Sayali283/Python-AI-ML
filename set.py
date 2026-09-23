b7d960fed7aa3dbf6922c2ce23f4deab1fee132c
Sayali1503 <ingolesayali283@gmail.com>
2026-09-23 13:04:00 +0530

    Add basic set operations in set.py
    Implement basic set operations including add, remove, union, intersection, difference, and symmetric difference.

---
+++ set.py
@@ -0,0 +1,17 @@
+A={1,2,3,4,5}
+B={6.,7,5,9,10}
+print(A)
+A.add(6) #Add
+print(A)
+A.remove(5) #remove
+print(A)
+U=A|B
+print(U) #union
+I=A&B
+print(I) #intersection
+D=A-B
+print(S) #difference
+S=A^B
+print(S) #symettric Difference
+print(5 in A) #Checking Membership
+print(len(A)) #length
